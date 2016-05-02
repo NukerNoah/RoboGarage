db8feec3c9c17eefb4c3427a3daa8ed4ec423bae
NukerNoah <noahcduke@gmail.com>
2016-05-01 22:15:16 -0500

    0.2.2 - Tiny Robots *Working*
    +Added custom graphics for the Construction and Logistic robot entities
    to be more size appropriate for the RoboGarage.
    +Fixed bad sound pointers caused by a bad sync of info.json from 2.0.0

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -89,7 +89,7 @@ data:extend({
     vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
     working_sound =
     {
-      sound = { filename = "__base__/sound/robo-garage-working.ogg", volume = 0.6 },
+      sound = { filename = "__base__/sound/roboport-working.ogg", volume = 0.6 },
       max_sounds_per_type = 3,
       audible_distance_modifier = 0.5,
       probability = 1 / (5 * 60) -- average pause between the sound is 5 seconds
@@ -115,15 +115,301 @@ data:extend({
     {
       {
         type = "play-sound",
-        sound = { filename = "__base__/sound/robo-garage-door.ogg", volume = 1.2 }
+        sound = { filename = "__base__/sound/roboport-door.ogg", volume = 1.2 }
       },
     },
     close_door_trigger_effect =
     {
       {
         type = "play-sound",
-        sound = { filename = "__base__/sound/robo-garage-door.ogg", volume = 0.75 }
+        sound = { filename = "__base__/sound/roboport-door.ogg", volume = 0.75 }
       },
     },
+  },
+  {
+    type = "logistic-robot",
+    name = "logistic-robot",
+    icon = "__base__/graphics/icons/logistic-robot.png",
+    flags = {"placeable-player", "player-creation", "placeable-off-grid", "not-on-map"},
+    minable = {hardness = 0.1, mining_time = 0.1, result = "logistic-robot"},
+    max_health = 100,
+    collision_box = {{0, 0}, {0, 0}},
+    selection_box = {{-0.5, -1.5}, {0.5, -0.5}},
+    max_payload_size = 1,
+    speed = 0.05,
+    transfer_distance = 0.5,
+    max_energy = "300kJ",
+    energy_per_tick = "0.01kJ",
+    speed_multiplier_when_out_of_energy = 0.2,
+    energy_per_move = "1kJ",
+    min_to_charge = 0.2,
+    max_to_charge = 0.95,
+    idle =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot.png",
+      priority = "high",
+      line_length = 16,
+      width = 27,
+      height = 28,
+      frame_count = 1,
+      shift = {0.015625, -0.09375},
+      direction_count = 16,
+      y = 28
+    },
+    idle_with_cargo =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot.png",
+      priority = "high",
+      line_length = 16,
+      width = 27,
+      height = 28,
+      frame_count = 1,
+      shift = {0.015625, -0.09375},
+      direction_count = 16
+    },
+    in_motion =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot.png",
+      priority = "high",
+      line_length = 16,
+      width = 27,
+      height = 28,
+      frame_count = 1,
+      shift = {0.015625, -0.09375},
+      direction_count = 16,
+      y = 72
+    },
+    in_motion_with_cargo =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot.png",
+      priority = "high",
+      line_length = 16,
+      width = 27,
+      height = 28,
+      frame_count = 1,
+      shift = {0.015625, -0.09375},
+      direction_count = 16,
+      y = 56
+    },
+    shadow_idle =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
+      priority = "high",
+      line_length = 16,
+      width = 45,
+      height = 18,
+      frame_count = 1,
+      shift = {0.96875, 0.609375},
+      direction_count = 16,
+      y = 18
+    },
+    shadow_idle_with_cargo =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
+      priority = "high",
+      line_length = 16,
+      width = 45,
+      height = 18,
+      frame_count = 1,
+      shift = {0.96875, 0.609375},
+      direction_count = 16
+    },
+    shadow_in_motion =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
+      priority = "high",
+      line_length = 16,
+      width = 45,
+      height = 18,
+      frame_count = 1,
+      shift = {0.96875, 0.609375},
+      direction_count = 16,
+      y = 18
+    },
+    shadow_in_motion_with_cargo =
+    {
+      filename = "__RoboGarage__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
+      priority = "high",
+      line_length = 16,
+      width = 45,
+      height = 18,
+      frame_count = 1,
+      shift = {0.96875, 0.609375},
+      direction_count = 16
+    },
+    working_sound = flying_robot_sounds(),
+    cargo_centered = {0.0, 0.2},
+  },
+
+  {
+    type = "construction-robot",
+    name = "construction-robot",
+    icon = "__base__/graphics/icons/construction-robot.png",
+    flags = {"placeable-player", "player-creation", "placeable-off-grid", "not-on-map"},
+    minable = {hardness = 0.1, mining_time = 0.1, result = "construction-robot"},
+    max_health = 100,
+    collision_box = {{0, 0}, {0, 0}},
+    selection_box = {{-0.5, -1.5}, {0.5, -0.5}},
+    max_payload_size = 1,
+    speed = 0.06,
+    transfer_distance = 0.5,
+    max_energy = "300kJ",
+    energy_per_tick = "0.01kJ",
+    speed_multiplier_when_out_of_energy = 0.2,
+    energy_per_move = "1kJ",
+    min_to_charge = 0.2,
+    max_to_charge = 0.95,
+    working_light = {intensity = 0.8, size = 3},
+    idle =
+    {
+      filename = "__RoboGarage__/graphics/entity/construction-robot/construction-robot.png",
+      priority = "high",
+      line_length = 16,
+      width = 24,
+      height = 27,
+      frame_count = 1,
+      shift = {0, -0.15625},
+      direction_count = 16
+    },
+    in_motion =
+    {
+      filename = "__RoboGarage__/graphics/entity/construction-robot/construction-robot.png",
+      priority = "high",
+      line_length = 16,
+      width = 24,
+      height = 27,
+      frame_count = 1,
+      shift = {0, -0.15625},
+      direction_count = 16,
+      y = 27
+    },
+    shadow_idle =
+    {
+      filename = "__RoboGarage__/graphics/entity/construction-robot/construction-robot-shadow.png",
+      priority = "high",
+      line_length = 16,
+      width = 37,
+      height = 18,
+      frame_count = 1,
+      shift = {1.09375, 0.59375},
+      direction_count = 16
+    },
+    shadow_in_motion =
+    {
+      filename = "__RoboGarage__/graphics/entity/construction-robot/construction-robot-shadow.png",
+      priority = "high",
+      line_length = 16,
+      width = 37,
+      height = 18,
+      frame_count = 1,
+      shift = {1.09375, 0.59375},
+      direction_count = 16
+    },
+    working =
+    {
+      filename = "__RoboGarage__/graphics/entity/construction-robot/construction-robot-working.png",
+      priority = "high",
+      line_length = 2,
+      width = 21,
+      height = 27,
+      frame_count = 2,
+      shift = {0, -0.15625},
+      direction_count = 16,
+      animation_speed = 0.3,
+    },
+    shadow_working =
+    {
+      stripes = util.multiplystripes(2,
+      {
+        {
+          filename = "__RoboGarage__/graphics/entity/construction-robot/construction-robot-shadow.png",
+          width_in_frames = 16,
+          height_in_frames = 1,
+        }
+      }),
+      priority = "high",
+      width = 37,
+      height = 18,
+      frame_count = 2,
+      shift = {1.09375, 0.59375},
+      direction_count = 16
+    },
+    smoke =
+    {
+      filename = "__base__/graphics/entity/smoke-construction/smoke-01.png",
+      width = 39,
+      height = 32,
+      frame_count = 19,
+      line_length = 19,
+      shift = {0.078125, -0.15625},
+      animation_speed = 0.3,
+    },
+    sparks =
+    {
+      {
+        filename = "__base__/graphics/entity/sparks/sparks-01.png",
+        width = 39,
+        height = 34,
+        frame_count = 19,
+        line_length = 19,
+        shift = {-0.109375, 0.3125},
+        tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
+        animation_speed = 0.3,
+      },
+      {
+        filename = "__base__/graphics/entity/sparks/sparks-02.png",
+        width = 36,
+        height = 32,
+        frame_count = 19,
+        line_length = 19,
+        shift = {0.03125, 0.125},
+        tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
+        animation_speed = 0.3,
+      },
+      {
+        filename = "__base__/graphics/entity/sparks/sparks-03.png",
+        width = 42,
+        height = 29,
+        frame_count = 19,
+        line_length = 19,
+        shift = {-0.0625, 0.203125},
+        tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
+        animation_speed = 0.3,
+      },
+      {
+        filename = "__base__/graphics/entity/sparks/sparks-04.png",
+        width = 40,
+        height = 35,
+        frame_count = 19,
+        line_length = 19,
+        shift = {-0.0625, 0.234375},
+        tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
+        animation_speed = 0.3,
+      },
+      {
+        filename = "__base__/graphics/entity/sparks/sparks-05.png",
+        width = 39,
+        height = 29,
+        frame_count = 19,
+        line_length = 19,
+        shift = {-0.109375, 0.171875},
+        tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
+        animation_speed = 0.3,
+      },
+      {
+        filename = "__base__/graphics/entity/sparks/sparks-06.png",
+        width = 44,
+        height = 36,
+        frame_count = 19,
+        line_length = 19,
+        shift = {0.03125, 0.3125},
+        tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
+        animation_speed = 0.3,
+      },
+    },
+    repair_pack = "repair-pack",
+    working_sound = flying_robot_sounds(),
+    cargo_centered = {0.0, 0.2},
+    construction_vector = {0.30, 0.22},
   }
- })
+})
